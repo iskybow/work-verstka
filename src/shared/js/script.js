5e7247ecabc935e77b70964d11e4e22c45a11149
iskybow <kjionmd@gmail.com>
2019-05-24 14:50:11 +0300

    главная

--- src/shared/js/script.js
+++ src/shared/js/script.js
@@ -63,14 +63,32 @@ if ($('.home__slider').length > 0) {
   $('.home__slider').slick({
     verticalSwiping: true,
     vertical: true,
-    arrows: false,
-    slidesToShow: 2,
+    arrows: true,
+    prevArrow: '<button type="button" class="prevSlickHome"><img src="assets/images/arrow-down.svg"></button>',
+    nextArrow: '<button type="button" class="nextSlickHome"><img src="assets/images/arrow-up.svg"></button>',
+    slidesToShow: 3,
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [
       {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
         breakpoint: 576,
         settings: {
+          arrows: false,
           slidesToShow: 1,
           slidesToScroll: 1,
         }
